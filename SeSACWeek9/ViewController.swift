742ef472495d3fef3ceab108d94cde895ac96902
skyler <ho971122@gmail.com>
2022-08-30 22:26:17 +0900

    URLSession API를 통한 데이터 요청 및 전송

--- SeSACWeek9/ViewController.swift
+++ SeSACWeek9/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
 
         }
         
-        PersonAPIManager.requestPerson(query: "lee") { person, errer in
+        PersonAPIManager.requestPerson(query: "won") { person, errer in
             guard let person = person else {
                 return
             }
